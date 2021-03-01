58f3324114e8f2dad47c9c6a2e5e1581d2348d20
Georgi Ditsov <g.ditsov@proxiad.com>
2021-03-01 09:35:20 +0200

    Adding missing @Repository annotations

--- src/main/java/com/ditsov/basicschoolgradingsystem/repository/student/StudentRepository.java
+++ src/main/java/com/ditsov/basicschoolgradingsystem/repository/student/StudentRepository.java
@@ -1,9 +1,11 @@
 package com.ditsov.basicschoolgradingsystem.repository.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ditsov.basicschoolgradingsystem.model.student.Student;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    
+
 }
